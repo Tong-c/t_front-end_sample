b084b703982252131f3f808d5e56483dc613e78c
tong <1972376180@qq.com>
2018-12-02 17:05:29 +0800

    1.t_vue_sample:仿去哪網項目首頁頭部、輪播

--- t_vue_sample/04/travel/src/pages/home/components/Header.vue
+++ t_vue_sample/04/travel/src/pages/home/components/Header.vue
@@ -1,35 +1,56 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">內容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe735;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe720;</span>
+      輸入城市/景點/遊玩主題</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe731;</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeHeader'
-};
+}
 </script>
 
 <style lang="stylus" scoped>
+@import '~@/assets/styles/variable.styl'
 .header {
   display: flex;
-  height: 0.86rem;
-  background: #00bcd4;
-
+  line-height: 0.86rem;
+  background: $bgColor;
+  color:#fff
   .header-left {
     width: 0.64rem;
     float: left;
+    .back-icon {
+      text-align : center;
+      font-size : .4rem;
+    }
   }
 
   .header-input {
     flex: 1;
+    height : .64rem
+    line-height : .64rem
+    margin-top : .12rem
+    margin-left : .2rem
+    padding-left : .2rem
+    background : #fff
+    border-radius : .1rem
+    color : #ccc
   }
 
   .header-right {
     width: 1.24rem;
     float: right;
+    text-align : center
   }
 }
 </style>
---
+++ t_vue_sample/04/travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrap">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl"/>
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/b7/1e9c4ca462741f02.jpg_750x200_842d5261.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/e0/7aed5dde53dabe02.jpg_750x200_92afe34c.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrap >>> .swiper-pagination-bullet-active
+        background : #fff
+    .wrap {
+        overflow : hidden;
+        width : 100%;
+        height : 0;
+        padding-bottom : 31.25%;
+        background : #eee;
+    }
+        .swiper-img {
+            width : 100%
+        }
+</style>
