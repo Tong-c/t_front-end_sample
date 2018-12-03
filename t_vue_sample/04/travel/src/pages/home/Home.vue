4591e63c729a4a6642983abdcb61db7b46a4da02
tong <1972376180@qq.com>
2018-12-03 21:18:05 +0800

    1.t_vue_saqmple:仿去哪网项目首页图标区域、推荐部分

--- t_vue_sample/04/travel/src/pages/home/Home.vue
+++ t_vue_sample/04/travel/src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -12,13 +13,28 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+  },
+  mounted () {
+
   }
 }
 </script>
--- t_vue_sample/04/travel/src/pages/home/components/Recommend.vue
+++ t_vue_sample/04/travel/src/pages/home/components/Recommend.vue
@@ -4,11 +4,11 @@
             热销推荐
         </div>
         <ul>
-            <li class="item">
-                <img class="item-img" src="">
+            <li class="item" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src="item.imgUrl">
                 <div class="item-info">
-                    <p class="item-title"></p>
-                    <p class="item-desc"></p>
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
                     <button class="item-button">查看详情</button>
                 </div>
             </li>
@@ -18,11 +18,37 @@
 
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1706/f1/f132dc663abd9175a3.img.jpg_200x200_2cd95301.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1706/f1/f132dc663abd9175a3.img.jpg_200x200_2cd95301.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1706/f1/f132dc663abd9175a3.img.jpg_200x200_2cd95301.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1706/f1/f132dc663abd9175a3.img.jpg_200x200_2cd95301.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题公园'
+      }]
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
     .title {
         margin-top: .2rem
         line-height: .8rem
@@ -33,11 +59,33 @@ export default {
         overflow: hidden
         display: flex
         height: 1.9rem
-        background: red
     }
         .item-img {
             width: 1.7rem
             height: 1.7rem
             padding: .1rem
         }
+        .item-info {
+            flex: 1
+            padding: .1rem
+            min-width: 0
+        }
+            .item-title {
+                line-height: .54rem
+                font-size: .32rem
+                ellipsis()
+            }
+            .item-desc {
+                line-height: .4rem
+                color: #ccc
+                ellipsis()
+            }
+            .item-button {
+                line-height: .44rem
+                margin-top: .16rem
+                background: #ff9300
+                padding: 0 .1rem
+                border-radius: .06rem
+                color: #fff
+            }
 </style>
